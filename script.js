bc7713148af6679fadfe4f437f4f3ab9f4777ee9
בר הורוביץ <brhwrwbyz@brs-MacBook-Pro.local>
2023-07-01 12:43:28 +0300

    js login

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
 const subMenuInfo = document.getElementById("subMenuInfo");
 const subMenuDynamic = document.getElementById("subMenuStart");
-let c = 0;
 
 function toggleMenu(){
-  if (c%2==2) {
-    subMenuInfo.style.display = "block";
-    subMenuDynamic.style.display = "none";
-  } else if (c%2==0) {
-    subMenuInfo.style.display = "none";
-    subMenuDynamic.style.display = "block";
-  } else {
-    console.log("none of them")
-    // Invalid menu choice
-    subMenuInfo.style.display = "none";
-    subMenuDynamic.style.display = "none";
-  }
-  subMenu.classList.toggle("open-menu");
+    var userName = localStorage.getItem("userName");
+    if (userName!=null & userName!="null") {
+        subMenuInfo.style.display = "block";
+        subMenuDynamic.style.display = "none";
+        document.getElementById("userNameMenu").textContent = userName;
+    } else if (userName==null || userName=="null") {
+        subMenuInfo.style.display = "none";
+        subMenuDynamic.style.display = "block";
+    }
+    subMenu.classList.toggle("open-menu");
+}
+
+const button = document.getElementById("loginbtn");
+if(button){
+    document.querySelector("#loginbtn").addEventListener("click",function(){
+        var userName = document.getElementById("inputUserName").value;
+        localStorage.setItem("userName",userName);
+    })
+}
+
+const a = document.getElementById("logOutDrop");
+if(a){
+    document.querySelector("#logOutDrop").addEventListener("click",function(){
+        localStorage.setItem("userName",null);
+       location.reload();
+    })
 }
